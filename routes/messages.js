11f641816c40cb0c55e54380e13290c012b06954
Valeriy Svydenko <vsvydenk@redhat.com>
2025-04-23 13:00:50 +0000

    chore: update image
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- routes/messages.js
+++ routes/messages.js
@@ -5,7 +5,7 @@
 
 const mongoose = require('mongoose')
 
-const mongoURI = "mongodb://user:password@localhost:27017/guestbook"
+const mongoURI = process.env.MONGODB_URI || 'mongodb://user:password@mongo:27017/guestbook';
 
 const db = mongoose.connection;
 
